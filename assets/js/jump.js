ffd9e3dcbe2fab8ecb835726769a4bd8fb60191f
Tyler <tdoermann@gmail.com>
2017-09-22 16:24:02 -0600

    cleaner

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -3,7 +3,7 @@ function jump(target, options) {
         start = window.pageYOffset,
         opt = {
             duration: options.duration,
-            offset: options.offset || -120,
+            offset: options.offset || -20,
             callback: options.callback,
             easing: options.easing || easeInOutQuad
         },
