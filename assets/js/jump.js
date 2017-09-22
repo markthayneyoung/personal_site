a61666d50cf506d96f4c4e68e655f9364d47d995
Tyler <tdoermann@gmail.com>
2017-09-22 14:30:30 -0600

    easy scroll

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -3,7 +3,7 @@ function jump(target, options) {
         start = window.pageYOffset,
         opt = {
             duration: options.duration,
-            offset: options.offset || -180,
+            offset: options.offset || -120,
             callback: options.callback,
             easing: options.easing || easeInOutQuad
         },
